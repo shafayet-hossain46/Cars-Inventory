395c02fa98b73330bf31c7c93d6b404c2486e3c8
shafayet-hossain46 <shafayethossaing46@gmail.com>
2022-08-02 09:21:02 +0600

    added myItems

--- src/pages/MyItems/MyItems.js
+++ src/pages/MyItems/MyItems.js
@@ -1,10 +1,104 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../Firebase/Firebase.init';
+import { Button, Card, Col, Container } from 'react-bootstrap';
 
 const MyItems = () => {
+    const [user, loading, error] = useAuthState(auth);
+    const [item, setItem] = useState([])
+
+    
+        useEffect(()=>{
+            axios.get(`http://localhost:5000/myItems?email=${user?.email}`)
+            .then(res => {
+                console.log(res.data);
+                setItem(res.data)
+            })
+        }, [user])
+
+
+
+        const handleDelete = (id) => {
+            axios.delete(`http://localhost:5000/inventoryItem/${id}`).then((res) => {
+              if (res.data.deletedCount > 0) {
+                const newItems = item.filter((item) => item._id !== id);
+                setItem(newItems);
+              }
+            });
+          };
+        
+    
     return (
-        <div>
-            This is my items
+        <Container>
+            <div className="col-md-8 row">
+          <h2 className="text-center mb-4 text-info">Manage Inventory Items</h2>
+          {item.map((item) => (
+            <Col className="col-md-4 col-12">
+              <Card
+                style={{
+                  boxShadow:
+                    "rgba(136, 165, 191, 0.48) 6px 2px 16px 0px, rgba(255, 255, 255, 0.8) -6px -2px 16px 0px",
+                }}
+                className="p-2 mb-4"
+              >
+                <Card.Img variant="top" className="img-fluid" src={item.img} />
+                <Card.Body>
+                  <Card.Title style={{ color: "#2C3A47" }}>
+                    {item.name}
+                  </Card.Title>
+                  <Card.Text>
+                    <small>
+                      <span style={{ color: "#34e7e4", fontWeight: "bold" }}>
+                        Supplier
+                      </span>
+                      : {item.supplier}
+                    </small>
+                    <br />
+                    <small>
+                      {" "}
+                      <span style={{ color: "#34e7e4", fontWeight: "bold" }}>
+                        Price $
+                      </span>
+                      {item.price}
+                    </small>
+                    <br />
+                    <small>
+                      <span style={{ color: "#34e7e4", fontWeight: "bold" }}>
+                        Quantity
+                      </span>{" "}
+                      {item.quantity}
+                    </small>
+                    <br />
+                  </Card.Text>
+
+                  <div className="d-grid gap-2">
+                    <Button
+                      onClick={() => handleDelete(item._id)}
+                      className="text-white"
+                      variant="danger"
+                      size="lg"
+                    >
+                      Delete Item
+                    </Button>
+                  </div>
+                  {/* <div className="d-grid gap-2 mt-2">
+                    <Button
+                      as={Link}
+                      to={`/manageInventoryItem/${item._id}`}
+                      className="text-white"
+                      variant="info"
+                      size="lg"
+                    >
+                      Manage
+                    </Button>
+                  </div> */}
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
         </div>
+        </Container>
     );
 };
 
